692a4b08cf9724c49c12059cf3761361a847b92e
panavrin <sangwonlee@0587361021.wireless.umich.net>
2015-04-06 16:25:55 -0400

    name creation

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # tindermusic
 tinder-audience-participation
+
+case 1 - creating name ( from one audience member to performer)
+
+Send: 
+{
+	type : "create", 
+	my_id : [unique my_id generated from audience.js],
+	nickname : [user typed id, alphabets, numbers, dot, underscore allowed, shorter than 12. ]
+}
+
+Respond: 
+{
+	type : "create-respond", 
+	...
+}
--- js/audience.js
+++ js/audience.js
@@ -1,44 +1,76 @@
 //alert("test");
 
 // PubNub code
+// Get an unique pubnub id
+var state = "NAME"; // it is either NAME, EDIT, PLAY
 
-  // Get an unique pubnub id
-  var my_id = PUBNUB.uuid();
-
-  // Initialize with Publish & Subscribe Keys
-  var pubnub = PUBNUB.init({
-      publish_key: 'demo',
-      subscribe_key: 'demo',
-      uuid: my_id
-  });
-
-  // Subscribe to a channel
-  pubnub.subscribe({
-      channel: my_id,
-      message: parseMessage,
-      error: function (error) {
-       // Handle error here
-       console.log(JSON.stringify(error));
-      }
-  });
-
-  // Parse messages received from PubNub platform
-  function parseMessage( message ) {
-      if (typeof message.nextDivName !== 'undefined') {
-        setNextDivName(message.nextDivName);
-      } else {
-        console.log(JSON.stringify(message))
-      }
+//This segment displays the validation rule for address field.
+function textAlphanumeric(inputtext){
+  var alphaExp = /^[0-9a-zA-Z._]+$/;
+  if(inputtext.match(alphaExp)){
+    return true;
+  }else{
+    return false;
   }
+}
+
+var my_id = PUBNUB.uuid();
+
+// Initialize with Publish & Subscribe Keys
+var pubnub = PUBNUB.init({
+    publish_key: 'demo',
+    subscribe_key: 'demo',
+    uuid: my_id
+});
+
+// Subscribe to a channel
+pubnub.subscribe({
+    channel: my_id,
+    message: parseMessage,
+    error: function (error) {
+     // Handle error here
+     console.log(JSON.stringify(error));
+    }
+});
+
+
+function parseMessage( message ) {
+    console.log("message - received:" + JSON.stringify(message));
+    if (typeof message.nextDivName !== 'undefined') {
+      setNextDivName(message.nextDivName);
+    } 
+    else if (typeof message.type !== 'undefined'){
+      if ( message.type == "create-respond"){
+        if (message.result == "s"){ 
+          state = "EDIT"; 
+          $('#initial-message').bPopup().close();  
+        }
+        else
+        {
+          $('#name_error_msg').text($('#screenname').val() + " is already taken.");
+        }
+      }
+    }
+    else {
+      console.log(JSON.stringify(message));
+    }
+}
 
   // Request the next div
   function getNextDivName() {
       var actualTindered =  document.getElementById('tindered');
       var actualDivName = actualTindered.textContent;
       pubnub.publish({
-            channel: "performer",
-            message: {"nextToDivName": actualDivName, "user": my_id}
+        channel: "performer",
+        message: {"nextToDivName": actualDivName, "user": my_id}
       });
+  }
+
+  function publishMessage(channel, options){
+    pubnub.publish({
+            channel: channel,
+            message: options
+    });
   }
 
   // Set the name of the next div
@@ -47,3 +79,40 @@
       actualTindered.innerHTML = "";
       actualTindered.appendChild(document.createTextNode(divName));
   }
+
+
+$(document).ready(function () {
+
+  // Parse messages received from PubNub platform
+  
+  $('#initial-message').bPopup({
+    modalClose: false,
+    opacity: 0.7,
+  //  positionStyle: 'fixed',
+  //  position: [50%, 20], 
+    escClose :false
+  });
+
+  $("#start").button().css({ margin:'5px'}).click(function(){
+      $("#name_error_msg").text("");
+
+    var strScreenName = $("#screenname").val();
+    if ( strScreenName.length > 12) {
+      $("#name_error_msg").text("screen name is too long");
+      return;
+    }
+
+    if ( textAlphanumeric(strScreenName) == false ) {
+      $("#name_error_msg").text("Please, use combination of alphabets and numbers for the screen name. ");
+      return;
+    }
+
+    publishMessage("performer", {"type":"create", "my_id":my_id, "nickname": strScreenName});
+
+    $("#name_error_msg").text("Waiting for response...");
+
+    // $('#initial-message').bPopup().close();
+  });
+
+
+});
