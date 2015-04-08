030d7c562b8c461443692fc7bd954ffa8d287a95
panavrin <sangwonlee@0587352624.wireless.umich.net>
2015-04-08 15:36:03 -0400

    synth changed to scissorvoice
    noisehack.com/scissor/

--- js/audience.js
+++ js/audience.js
@@ -3,7 +3,110 @@
 // PubNub code
 // Get an unique pubnub id
 var state = "NAME"; // it is either NAME, EDIT, PLAY
-var soundEnabled = false;
+var soundEnabled = true;
+var context;
+var compressor;
+
+function getRandomInt (min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function noteNum2Freq(num){
+    return Math.pow(2,(num-57)/12) * 440 
+}
+
+if(soundEnabled){
+  try {
+    // still needed for Safari
+    window.AudioContext = window.AudioContext || window.webkitAudioContext;
+    // create an AudioContext
+    //context = WX._ctx
+    context = new window.AudioContext();
+   // alert('Web Audio API supported.');
+    compressor = context.createDynamicsCompressor()
+  } catch(e) {
+    // API not supported
+    alert('Web Audio API not supported, please use most recent Chrome (41+), FireFox(31+) or Safari (iOS 7.1+).');
+  }
+
+}
+
+function ADSR(){
+    this.node = context.createGain();
+    this.node.gain.value = 0.0;
+}
+
+ADSR.prototype.noteOn= function(delay, A,D, peakLevel, sustainlevel){
+    peakLevel = peakLevel || 1;
+    sustainlevel = sustainlevel || 0.3;
+    
+    this.node.gain.linearRampToValueAtTime(0.0,delay + context.currentTime);
+    this.node.gain.linearRampToValueAtTime(peakLevel,delay + context.currentTime + A); // Attack
+    this.node.gain.linearRampToValueAtTime(sustainlevel,delay + context.currentTime + A + D);// Decay
+}
+
+ADSR.prototype.noteOff= function(delay, R, sustainlevel){
+    sustainlevel = sustainlevel || 0.1;
+
+    this.node.gain.linearRampToValueAtTime(sustainlevel,delay + context.currentTime );// Release   
+    this.node.gain.linearRampToValueAtTime(0.0,delay + context.currentTime + R);// Release   
+    
+}
+
+ADSR.prototype.play= function(delay, A,D,S,R, peakLevel, sustainlevel){
+  this.node.gain.linearRampToValueAtTime(0.0,delay + context.currentTime);
+  this.node.gain.linearRampToValueAtTime(peakLevel,delay + context.currentTime + A); // Attack
+  this.node.gain.linearRampToValueAtTime(sustainlevel,delay + context.currentTime + A + D);// Decay
+  this.node.gain.linearRampToValueAtTime(sustainlevel,delay + context.currentTime + A + D + S);// sustain.
+  this.node.gain.linearRampToValueAtTime(0.0,delay + context.currentTime + A + D + S + R);// Release   
+}
+var index = 0;
+
+function ScissorVoice(noteNum, numOsc, oscType, detune){
+  this.output  = new ADSR();
+  this.maxGain = 1 / numOsc;
+  this.noteNum = noteNum;
+  this.frequency = noteNum2Freq(noteNum);
+  this.oscs = [];
+  this.index = index++;
+  this.time = context.currentTime;
+  for (var i=0; i< numOsc; i++){
+    var osc = context.createOscillator();
+    osc.type = oscType;
+    osc.frequency.value = this.frequency;
+    osc.detune.value = -detune + i * 2 * detune / (numOsc - 1);
+    osc.start(context.currentTime);
+    osc.connect(this.output.node);
+    this.oscs.push(osc);
+  }
+  //console.log("played(" + index +") " + noteNum + " at " + context.currentTime);
+   //   console.log("started : " +this.noteNum);
+
+}
+
+ScissorVoice.prototype.stop = function(time){
+  //time =  time | context.currentTime;
+  var it = this;
+  setTimeout(function(){
+ //   console.log("stopped(" + index +") " +it.noteNum + " at " +context.currentTime);
+    for (var i=0; i<it.oscs.length; i++){
+        it.oscs[i].disconnect();
+    }
+  }, Math.floor((time-context.currentTime)*1000));
+}
+
+ScissorVoice.prototype.detune = function(detune){
+    for (var i=0; i<this.oscs.length; i++){
+        //this.oscs[i].frequency.value = noteNum2Freq(noteNum);
+        this.oscs[i].detune.value -= detune;
+    }
+}
+
+ScissorVoice.prototype.connect = function(target){
+  this.output.node.connect(target);
+}   
+
+
 
 window.requestAnimFrame = (function(){
 return  window.requestAnimationFrame       || 
@@ -154,31 +257,12 @@ $(document).ready(function () {
   var pattern = [];
   var patternSize = 5;
 
-  var context;
   // this is moved here to support iOS : http://stackoverflow.com/questions/12517000/no-sound-on-ios-6-web-audio-api
-  if(soundEnabled){
-    try {
-      // still needed for Safari
-      window.AudioContext = window.AudioContext || window.webkitAudioContext;
-      // create an AudioContext
-      //context = WX._ctx
-      context = new window.AudioContext();
-     // alert('Web Audio API supported.');
-
-    } catch(e) {
-      // API not supported
-      alert('Web Audio API not supported, please use most recent Chrome (41+), FireFox(31+) or Safari (iOS 7.1+).');
-    }
-
-
-  //  var synth = WX.FMK1();
-   // var synth = WX.WXS1();
-  }
+  
 
 
   $("#start").button().css({ margin:'5px'}).click(function(){
-          alert("start button clicked");
-
+        
     $("#name_error_msg").text("");
 
     var strScreenName = $("#screenname").val();
@@ -208,24 +292,10 @@ $(document).ready(function () {
     }),*/
 
     if (soundEnabled){
-      alert("soundEnabled");
-      var converb = WX.ConVerb({
-      mix: 1.0,
-      output: 0.5
-      }), 
-      compressor = context.createDynamicsCompressor()
-      , masterGain = context.createGain();
+      
+      var  masterGain = context.createGain();
 
       masterGain.gain.value = 1.0;
-
-      converb.loadClip({
-        name: 'BigEmptyChurch',
-        url: './sound/960-BigEmptyChurch.mp3'
-      });
-
-      //synth.to(compressor);
-      synth.to(converb).to(compressor);
-
       masterGain.connect(context.destination);
       compressor.connect(masterGain);
     }
@@ -292,6 +362,7 @@ $(document).ready(function () {
     window.requestAnimFrame(animate);
 
     var currentTime = Date.now();
+    var intervalInSec = interval/1000;
 
     if (playBarNote < 0 && lastPingTime  + interval > currentTime)
       return;
@@ -299,24 +370,34 @@ $(document).ready(function () {
       playBarNote++;
       lastPingTime = currentTime;
       interval = pattern[playBarNote].distance / speed;
+      intervalInSec = interval/1000;
     //  synth.noteon(60, 127, context.currentTime);
     //  synth.noteoff(60,0,context.currentTime + 1);
       if (soundEnabled){
-        synth.onData('noteon', {"pitch":60, "time":context.currentTime});
-        synth.onData('noteoff', {"pitch":60, "time":context.currentTime+1});
-      } 
-    //    console.log("begin! (" + pattern[playBarNote].distance + "," + interval);
-    }
 
+        var voice  =  new ScissorVoice(60,3,"triangle", 12);
+           //drone = new ScissorVoice(pitchListforDrone[pitchIndex],getRandomInt(3,10),"triangle", [3,5,7,12][getRandomInt(0,3)]);
+        voice.stop(context.currentTime + intervalInSec * 0.7);
+        voice.connect(compressor);
+        //function(delay, A,D, peakLevel, sustainlevel)
+        //function(time, A,D,S,R, peakLevel, sustainlevel){
+        voice.output.play(0,intervalInSec*0.1,intervalInSec*0.1,intervalInSec*0.4,intervalInSec*0.1,voice.maxGain*2.0,voice.maxGain );
+
+        //voice.output.noteOn(0,intervalInSec*0.1,intervalInSec*0.5,voice.maxGain*2.0,voice.maxGain);
+        // ADSR.prototype.noteOff= function(delay, R, sustainlevel){
+        // voice.output.noteOff(intervalInSec*0.5, intervalInSec*0.5,voice.maxGain);    
+        
+    //    console.log("begin! (" + pattern[playBarNote].distance + "," + interval);
+      }
+    }
     progress = (currentTime - lastPingTime ) / interval;
     if (progress >=1){
       playBarNote++;
+      var pitch = 60+ pentatonicScale[playBarNote];
       progress = 0;
+
       lastPingTime = currentTime;
-      if ( soundEnabled){
-        synth.onData('noteon', {"pitch":60+ pentatonicScale[playBarNote], "time":context.currentTime});
-        synth.onData('noteoff', {"pitch":60+ pentatonicScale[playBarNote], "time":context.currentTime+1});
-      }//      synth.noteon(60 + pentatonicScale[playBarNote], 127, context.currentTime);
+      //      synth.noteon(60 + pentatonicScale[playBarNote], 127, context.currentTime);
   //    synth.noteoff(60 + pentatonicScale[playBarNote],0,context.currentTime + 1);
       
       if (playBarNote == patternSize-1)
@@ -327,6 +408,25 @@ $(document).ready(function () {
       }else{
         interval = pattern[playBarNote].distance / speed;
   //      console.log("next! (" + pattern[playBarNote].distance + "," + interval);
+      }
+      intervalInSec = interval/1000;
+      if ( soundEnabled){
+        //synth.onData('noteon', {"pitch":60+ pentatonicScale[playBarNote], "time":context.currentTime});
+        //synth.onData('noteoff', {"pitch":60+ pentatonicScale[playBarNote], "time":context.currentTime+1});
+        var voice  =  new ScissorVoice(pitch,3,"triangle", 3);
+           //drone = new ScissorVoice(pitchListforDrone[pitchIndex],getRandomInt(3,10),"triangle", [3,5,7,12][getRandomInt(0,3)]);
+        //console.log("currentTime:" + context.currentTime);
+        //voice.stopAt = context.currentTime + intervalInSec * 0.4;
+        
+        voice.stop( context.currentTime + intervalInSec * 0.7);
+        
+        voice.connect(compressor);
+        voice.output.play(0,intervalInSec*0.1,intervalInSec*0.1,intervalInSec*0.4,intervalInSec*0.1,voice.maxGain*2.0,voice.maxGain );
+        //function(delay, A,D, peakLevel, sustainlevel)
+       // voice.output.noteOn(0,intervalInSec*0.1,intervalInSec*0.5,voice.maxGain*2.0,voice.maxGain);
+        // ADSR.prototype.noteOff= function(delay, R, sustainlevel){
+       // voice.output.noteOff(intervalInSec*0.5, intervalInSec*0.5,voice.maxGain);    
+        
       }
     }
 
