98cdd0029deae844f69172488e6d4c0e5b274393
deusanyjunior <deusanyjunior@gmail.com>
2015-04-10 00:58:20 -0400

    all cases working

--- README.md
+++ README.md
@@ -1,32 +1,40 @@
 # tindermusic
 tinder-audience-participation
 
-case 1 - creating name ( from one audience member to performer)
+case 0 - performance status
 
-send:
+callback: (from performer to everybody when someone connect, or when performer press some button?!)
+{
+	type : "performance",
+	status : [started,stopped]	// wait can stop all
+}
+
+case 1 - creating name
+
+send: (from one audience member to performer)
 {
 	type : "create",
 	my_id : [unique my_id generated from audience.js],
 	nickname : [user typed id, alphabets, numbers, dot, underscore allowed, shorter than 12. ]
 }
 
-response if successful:
+response if successful: (from performer to one audience member)
 {
 	type : "create-response",
 	res : "s",
 	index : [number representing the index of the nick name on the table]
 }
 
-response if fail:
+response if fail: (from performer to one audience member)
 {
 	type : "create-response",
 	res : "f"
 }
 
 
-case 2 - update tinder complete ( from one audience member to performer)
+case 2 - update tinder complete
 
-send:
+send: (from one audience member to performer)
 {
 	type : "update",
 	index : [index returned on create-response],
@@ -34,7 +42,7 @@ send:
 }
 
 response when updated if
-- we have some tinder music is available:
+- we have some tinder music is available: (from performer to one audience member)
 {
 	type : "update-response",
 	suggested_tm : {
@@ -45,17 +53,61 @@ response when updated if
 }
 
 response when updated if
-- we don't have some tinder music available:
+- we don't have some tinder music available: (from performer to one audience member)
 {
 	type : "update-response",
 	suggested_tm : ""
 }
 
 
+case 3 - request next tinder music
 
-case a - user goes to edit mode and send message to the followers (from performer)
-
-send:
+send: (from one audience member to performer)
 {
-	type: "user-unavailable"
+	type : "next",
+	index : [index returned on create-response]
+}
+
+response if we have some next tinder music: (from performer to one audience member)
+{
+	type : "next-response",
+	suggested_tm : {
+		nickname : [nickname from the owner of the tinder music],
+		index : [index from the owner of the tinder music],
+		tm: [string with the tinder music?!]
+		}
+}
+
+response if we don't have tinder music available: (from performer to one audience member)
+{
+	type : "next-response",
+	suggested_tm : ""
+}
+
+case 4 - go back to editing mode
+
+send: (from one audience member to performer)
+{
+	type : "editing",
+	index : [index returned on create-response]
+}
+
+response: (from performer to audience member)
+{
+	type: "editing-response",
+	tm: [string with the tinder music?!]
+}
+
+case 4 a - user goes to edit mode and send message to the followers
+
+send: (from performer to all followers of this audience member)
+{
+	type: "user-editing" // can I add the next tinder music here and save one message?!
+}
+
+case 5 - performer set user as unavailable and send message to the followers
+
+send: (from performer to all followers of this audience member)
+{
+	type: "user-unavailable" // can I add the next tinder music here and save one message?!
 }
--- js/performer.js
+++ js/performer.js
@@ -1,5 +1,7 @@
 // main code
 
+  var performanceStarted = false;
+
   var arrayUniqueNicknames = new Array();
   var arrayTinderMusics = new Array();
   var arrayAvailables = new Array();
@@ -32,8 +34,8 @@
 
   // subscribe to a channel
   pubnub.subscribe({
-    channel: 'performer',
-    presence: function(m){console.log(m)},
+    channel: 'performer,audience',
+    presence: performanceStatus,
     message: parseMessage,
     error: function (error) {
      // Handle error here
@@ -41,17 +43,54 @@
     }
   });
 
+  // send the performance status
+  function performanceStatus( message ) {
+    var status = null;
+    if (performanceStarted) {
+      status = "started";
+    } else {
+      status = "stopped";
+    }
+
+    // if the method is called from presence callback
+    if (typeof message.uuid !== 'undefined') {
+      pubnub.publish({
+            channel: message.uuid,
+            message: {"type": "performance",
+                      "status": status
+            }
+      });
+    // if the method is called from other place
+    } else {
+      for (i = 0, i < arrayTinderMusics.length; i++) {
+        pubnub.publish({
+              channel: arrayTinderMusics[i].id,
+              message: {"type": "performance",
+                        "status": status
+              }
+        });
+      }
+    }
+
+  }
+
   // parse messages received from PubNub platform
   function parseMessage( message ) {
     if (typeof message.type !== 'undefined') {
 
       switch(message.type) {
         case 'create':
-            create(message.my_id, message.nickname);
-            break;
+          create(message.my_id, message.nickname);
+          break;
         case 'update':
-            update(message.index, message.tm);
-            break;
+          update(message.index, message.tm);
+          break;
+        case 'next':
+          next(message.index);
+          break;
+        case 'editing':
+          editing(message.index);
+          break;
         default:
           break;
       }
@@ -60,21 +99,27 @@
     }
   }
 
+
+
+// Controller
+
   // create a user if possible
   function create(user_id, user_nickname) {
+    // if the nickname doesn't exist
     if ( arrayUniqueNicknames.indexOf(user_nickname) == -1 ) {
-      var index = arrayUniqueNicknames.push(user_nickname) - 1;
+      var index = arrayUniqueNicknames.push(user_nickname) - 1; // push returns the length
       var tm = {
         'id' : user_id,
         'nickname' : user_nickname,
-        'index' : index,
-        'tm' : "",
-        'mode' : "editing",
-        'status' : "unavailable",
+        'index' : index, // index at arrayUniqueNicknames will be the same in arrayTinderMusics (I hope!)
+        'tm' : "", // tinder music
+        'mode' : "editing", // modes: editing, following
+        'status' : "unavailable", // status: unavailable, available
         'follow' : "",
         'followers' : []
       };
       arrayTinderMusics.push(tm);
+      // add to view
       setAsUnavailable(index);
 
       pubnub.publish({
@@ -84,6 +129,7 @@
                       "index": index
             }
       });
+    // if the nickname already exists
     } else {
       pubnub.publish({
             channel: user_id,
@@ -93,43 +139,43 @@
     }
   }
 
+  // update the tinder music and get the next user to follow (get the next?! always?!)
   function update(user_index, user_tm) {
     var user = arrayTinderMusics[user_index];
+
+    // update the tinder music
     user.tm = user_tm;
+
+    // set the user mode
     user.mode = "following";
 
-    var suggested_index =  -1;
-    if ( arrayAvailables.length > 0) {
-      if ( user.follow == "" ) {
-            suggested_index = 0;
-      } else {
-        var ex_followed = user.follow;
+    if (user.status == 'unavailable') {
+      document.getElementById(user.index).className = 'unavailable';
+    } else {
+      document.getElementById(user.index).className = 'available';
+    }
 
-        // unfollow
-        follower_id = arrayTinderMusics[ex_followed].followers.indexOf[user.id];
-        if (follower_id > -1) {
-          arrayTinderMusics[ex_followed].followers.splice(user.id,1);
-        }
-        // TODO: show the number of followers
+    suggested_index = get_next_user_to_follow(user_index);
 
-        if ( arrayTinderMusics[ex_followed].status == 'available') {
-          if (arrayAvailables.indexOf(ex_followed) < (arrayAvailables.length-1)) {
-            suggested_index = ex_followed + 1;
-          } else {
-            suggested_index = 0;
-          }
-        } else {
-          suggested_index = 0;
-        }
+    if ( typeof(user.follow) == 'number' ) {
+      var ex_followed = user.follow;
+      // unfollow the older
+      var follower_id = arrayTinderMusics[ex_followed].followers.indexOf[user.index];
+      if (follower_id != -1) {
+        arrayTinderMusics[ex_followed].followers.splice(follower_id,1);
       }
+      // TODO: update the number of followers on the screen
+    }
+
+    if ( suggested_index != -1 ) {
+      var suggested = arrayTinderMusics[suggested_index];
 
       // follow
       user.follow = suggested_index;
-      arrayTinderMusics[suggested_index].followers.push(user.id);
+      suggested.followers.push(user.index);
       // TODO: show the number of followers
 
       // response
-      var suggested = arrayTinderMusics[arrayAvailables[suggested_index]];
       pubnub.publish({
             channel: user.id,
             message: {"type": "update-response",
@@ -151,10 +197,125 @@
             }
       });
     }
+
   }
+
+  // update the next user to follow
+  function next(user_index) {
+    var user = arrayTinderMusics[user_index];
+
+    suggested_index = get_next_user_to_follow(user_index);
+
+    if ( typeof(user.follow) == 'number' ) {
+      var ex_followed = user.follow;
+      // unfollow the older
+      var follower_id = arrayTinderMusics[ex_followed].followers.indexOf[user.index];
+      if (follower_id != -1) {
+        arrayTinderMusics[ex_followed].followers.splice(follower_id,1);
+      }
+      // TODO: update the number of followers on the screen
+    }
+
+    if ( suggested_index != -1 ) {
+      var suggested = arrayTinderMusics[suggested_index];
+
+      // follow
+      user.follow = suggested_index;
+      suggested.followers.push(user.index);
+      // TODO: update the number of followers on the screen
+
+      // response
+      pubnub.publish({
+            channel: user.id,
+            message: {"type": "next-response",
+                      "suggested_tm": {
+                        "nickname" : suggested.nickname,
+                        "index" : suggested.index,
+                        "tm" : suggested.tm
+                      }
+            }
+      });
+
+    } else {
+      // there is no user to follow
+      user.follow = "";
+      pubnub.publish({
+            channel: user.id,
+            message: {"type": "next-response",
+                      "suggested_tm": ""
+            }
+      });
+    }
+  }
+
+
+  // return the index of the next user to follow
+  // based on the index of the follower (not the index of the followed)
+  function get_next_user_to_follow(user_index) {
+    var user = arrayTinderMusics[user_index];
+
+    var suggested_index = -1;
+
+    if ( arrayAvailables.length > 0 ) {
+      suggested_index = arrayTinderMusics[ arrayAvailables[0] ].index;
+
+      if ( typeof(user.follow) == 'number' ) {
+        var ex_followed = user.follow;
+        // search the next one
+        if ( arrayTinderMusics[ex_followed].status == 'available') {
+          var possibleAvailable = arrayAvailables.indexOf(ex_followed) + 1;
+
+          for ( i = possibleAvailable; i < arrayAvailables.length; i++ ) {
+            if ( arrayTinderMusics[ arrayAvailables[i] ].mode != 'editing' ) {
+              suggested_index = arrayTinderMusics[ arrayAvailables[i] ].index;
+              break; // found!
+            } // else try next one
+          }
+        } // else there is no one available, follow the first
+      } // else the user is not following anybody, follow the first
+    } // else there is no one to follow, return -1
+
+    return suggested_index;
+  }
+
+
+  // when the user goes to edit mode
+  // the performer change the mode and color
+  // and inform to everybody
+  function editing(user_index) {
+    var user = arrayTinderMusics[user_index];
+
+    // change mode
+    user.mode = 'editing'
+
+    // chande the color
+    if (user.status == 'unavailable') {
+      document.getElementById(user.index).className = 'unavailable-editing';
+    } else {
+      document.getElementById(user.index).className = 'available-editing';
+    }
+
+    // reponse with the latest tinder music
+    pubnub.publish({
+          channel: user.id,
+          message: {"type": "editing-response",
+                    "tm" : user.tm
+          }
+    });
+
+    // inform the followers
+    for ( i = 0; i < user.followers.length; i++) {
+      pubnub.publish({
+            channel: arrayTinderMusics[ user.followers[i] ].id,
+            message: {"type": "user-editing"}
+      });
+    }
+  }
+
 
   // View
 
+  // set the tinder music as unavailable
   function setAsUnavailable(index) {
     var user = arrayTinderMusics[index];
 
@@ -172,17 +333,53 @@
 
     var newDiv = document.createElement('div');
     newDiv.id = index;
-    newDiv.className = 'unavailable';
-    newDiv.appendChild(document.createTextNode(arrayTinderMusics[index].nickname));
+    if ( user.mode != 'editing') {
+      newDiv.className = 'unavailable';
+    } else {
+      newDiv.className = 'unavailable-editing';
+    }
+    newDiv.appendChild(document.createTextNode(user.nickname));
+    // onclick set as available
     newDiv.onclick = function() {
-      divUnavailables.removeChild(this);
-      setAsAvailable(index);
+      // the user can go to available mode only when it is playing, yeah?!
+      if (arrayTinderMusics[index].mode != 'editing') {
+        var actualIndex = arrayUnavailables.indexOf(index);
+        if (actualIndex != -1) {
+          arrayUnavailables.splice(actualIndex,1);
+        }
+        divUnavailables.removeChild(this);
+        setAsAvailable(index);
+      }
     }
 
     arrayUnavailables.push(index);
     divUnavailables.appendChild(newDiv);
   }
 
+  // set the tinder music as available
   function setAsAvailable(index) {
+    var user = arrayTinderMusics[index];
 
+    user.status = 'available';
+
+    var newDiv = document.createElement('div');
+    newDiv.id = index;
+    if (user.mode != 'editing') {
+      newDiv.className = 'available';
+    } else {
+      newDiv.className = 'available-editing';
+    }
+    newDiv.appendChild(document.createTextNode(user.nickname));
+    // onclick, set as Unavailable
+    newDiv.onclick = function() {
+      var actualIndex = arrayAvailables.indexOf(index);
+      if (actualIndex != -1) {
+        arrayAvailables.splice(actualIndex,1);
+      }
+      divAvailables.removeChild(this);
+      setAsUnavailable(index);
+    }
+
+    arrayAvailables.push(index);
+    divAvailables.appendChild(newDiv);
   }
